3ee5eae701751b4aed389613f27075e30e7c06b4
indirahmaini <60458335+indirahmaini@users.noreply.github.com>
2020-01-30 11:31:11 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Data Peminjaman Buku Tanjung Balai</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/images.png"></img></p>
+                          <p class="label-title" align="center"><strong>Perpustakaan Umum</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Peminjaman Buku Tanjung Balai<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data peminjaman, pengelolaan peminjaman buku, <br>
+                            dan juga pengelolaan laporan serta peminjamannya.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data peminjaman buku.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
